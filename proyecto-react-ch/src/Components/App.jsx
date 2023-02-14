6e2d507143944bc3365af093c56f3272d74cfa6f
carloscudriz <carloscudriz21@gmail.com>
2023-02-14 16:45:33 -0500

    agregamos la card de los detalles especificos de cada producto

--- proyecto-react-ch/src/Components/App.jsx
+++ proyecto-react-ch/src/Components/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import  Navbar from './Navbar/Navbar';
 import { ItemListContainer } from './ItemListContainer/ItemListContainer';
+import { ItemDetailContainer } from './ItemDetailContainer/ItemDetailContainer';
 
 
 export const App = () => {
@@ -9,6 +10,7 @@ export const App = () => {
     <>
       <Navbar/> 
       <ItemListContainer/>   
+      <ItemDetailContainer/> 
     
     </>
   )
