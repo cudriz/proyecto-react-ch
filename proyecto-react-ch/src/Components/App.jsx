5e917cfb0b14e473b2cfcfab1cfc9353948119ee
carloscudriz <carloscudriz21@gmail.com>
2023-02-14 10:20:03 -0500

    agregamos un contador, para restar o sumar productos

--- proyecto-react-ch/src/Components/App.jsx
+++ proyecto-react-ch/src/Components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
-import ItemListContainer from "./ItemListContainer/ItemListContainer";
-
+// import ItemListContainer from "./ItemListContainer/ItemListContainer";
+import ItemCount from "./ItemCount/ItemCount";
 const App = () => {
   return (
     <>
       <Navbar />
-      <ItemListContainer greeting={"klk"}/>
+      <ItemCount ValInicial={1} stock={17}/>
+      {/* <ItemListContainer greeting={"klk"}/> */}
     </>
   );
 };
