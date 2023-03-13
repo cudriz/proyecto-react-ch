9a3e69b87b103681a5a9bd3a898d03a5f69bb641
carloscudriz <carloscudriz21@gmail.com>
2023-03-13 13:12:06 -0500

    agregamos el formulario de finalizar compra(componente ckeckout)

--- proyecto-react-ch/src/Components/App.jsx
+++ proyecto-react-ch/src/Components/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'; 
+import 'react-toastify/dist/ReactToastify.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer} from 'react-toastify';
 
 import  {Navbar} from './Navbar/Navbar';
 import { ItemListContainer } from './ItemListContainer/ItemListContainer';
 import { ItemDetailContainer } from './ItemDetailContainer/ItemDetailContainer';
+import { Checkout } from './Checkout/Checkout';
+import { Cart } from './Cart/Cart';
 
 
 export const App = () => {
@@ -16,7 +20,10 @@ export const App = () => {
           <Route path='/' element={<ItemListContainer/>}/>
           <Route path='/category/:idCategoria' element={<ItemListContainer/>}/>
           <Route path='/item/:id' element={<ItemDetailContainer/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
+          <Route path='/cart' element={<Cart/>}/>
         </Routes>   
+        <ToastContainer/>
       </BrowserRouter>  
     
     </>
