d0129f84d75dd2f236b58a159e6af899183b8693
carloscudriz <carloscudriz21@gmail.com>
2023-03-16 12:54:28 -0500

    apikey

--- proyecto-react-ch/src/utils/firebase.js
+++ proyecto-react-ch/src/utils/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, doc, addDoc, getDocs, getDoc, updateDoc, deleteDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3QvcBfbw5TLV6O0AiuaOZBBm8VUOHyLo",
+  apiKey: "process.env.API_KEY",
   authDomain: "proyecto-de-react-ch.firebaseapp.com",
   projectId: "proyecto-de-react-ch",
   storageBucket: "proyecto-de-react-ch.appspot.com",
